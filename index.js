a498c763b85e0428fac92ae220ae3d9d0e346107
Xinkai Wang <aakwxk@hotmail.com>
2014-07-04 12:44:20 -0700

    Rendering Less Templates

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var connect = require('connect');
 var serverStatic = require('serve-static');
 var makejade = require('./lib/processor/jade.js');
+var makeless = require('./lib/processor/less.js');
 
 function createMiniHarp(rootPath) {
   var app = connect();
   if (rootPath) {
     app.use(makejade(rootPath));
+    app.use(makeless(rootPath));
     app.use(serverStatic(rootPath));
   }
   return app;
