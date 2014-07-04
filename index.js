8f95924ae791420c52197442e43b36cbd18e278a
Xinkai Wang <aakwxk@hotmail.com>
2014-07-04 18:47:17 +0000

    jade.js need debug

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var connect = require('connect');
 var serverStatic = require('serve-static');
+var makejade = require('./lib/processor/jade.js');
 
 function createMiniHarp(rootPath) {
   var app = connect();
   if (rootPath) {
+    app.use(makejade(rootPath));
     app.use(serverStatic(rootPath));
   }
   return app;
