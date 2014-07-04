4390fd2331714febe088a3dcb1e4081fec74e66f
Xinkai Wang <aakwxk@hotmail.com>
2014-07-04 13:00:21 -0700

    Serving The Root Path

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var connect = require('connect');
 var serverStatic = require('serve-static');
+var defaultpage = require('./lib/processor/defaultpage.js');
 var makejade = require('./lib/processor/jade.js');
 var makeless = require('./lib/processor/less.js');
 
 function createMiniHarp(rootPath) {
   var app = connect();
   if (rootPath) {
+    app.use(defaultpage(rootPath));
     app.use(makejade(rootPath));
     app.use(makeless(rootPath));
     app.use(serverStatic(rootPath));
